f7b633415160810fbd232899855aa1fe7ef73214
Christopher-67 <christophers892@nycstudents.net>
2025-09-12 07:31:46 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ t = Turtle()
 
 t.shape('turtle')
 
-# t.forward(200)
+# """ t.forward(200)
 
 # def message(input):
 #      print(input)
@@ -55,10 +55,28 @@ t.shape('turtle')
 #      t.forward(x)
 #      t.left(90)
 #      t.forward(x)
-# equal(200)
+# equal(200) """
 
-for i in range(4):
-     t.forward(100)
-     t.left(90)
+# sidelength = 100
+# rotate = 90
+# def square(x,y):
+#  for i in range(4):
+#      t.forward(x)
+#      t.left(y)
+# triangle(100,90)
+# def manySquares(x):
+#       y = 10
+# for i in range(3):
+#       square(y)
+#       y = y + 5
+#       t.left(5)
+# manySquares(60)
 
-     turtle.done()
+def doublesquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+doublesquares(5)
+
+turtle.done()
